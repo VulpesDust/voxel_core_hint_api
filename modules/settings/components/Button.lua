5146d970f6ef1e00ed74509f2e25caabf4e853de
VulpesDust <noise13qwer@gmail.com>
2025-02-27 22:47:04 +0300

    Update Button.lua

--- modules/settings/components/Button.lua
+++ modules/settings/components/Button.lua
@@ -7,6 +7,8 @@ function this.draw(gui, parent_id, args)
             color = gui.style.btn_bg,
             ["hover-color"] = gui.style.btn_hover_bg,
             ["pressed-color"] = gui.style.btn_active_bg,
+            padding = 6,
+
             onclick = args.click
         }
     })
@@ -16,20 +18,13 @@ function this.draw(gui, parent_id, args)
         table.insert(lines, line)
     end
     for i, line in ipairs(lines) do
-        local margin_top = (i == 1) and 8 or 2
-        local margin_bottom = (i == #lines) and 8 or 2
-        local margin = string.format('8, %s, 8, %s', margin_top, margin_bottom)
         local label = gui.draw(gui, id, {
             tag = 'label',
             label = line,
             atr = {
-                -- size = '250, 150',
                 valign = 'center',
                 align = 'center',
-                -- autoresize = 'true',
-                -- multiline = 'true',
-                -- ["text-wrap"] = 'true',
-                margin = margin
+                margin = 2
             }
         })
     end
